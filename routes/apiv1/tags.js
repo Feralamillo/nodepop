55b5324c4e1c70f4bcd2f343e6f8ffff40244b34
fernando <fernando@ongoney.com>
2018-02-18 10:09:46 +0100

    including tags.js in apiv1

---
+++ routes/apiv1/tags.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const express = require("express");
+const router = express.Router();
+
+const Anuncio = require("../../models/Anuncio");
+
+router.get("/", (req, res, next) => {
+  Anuncio.find()
+    .exec()
+    .then(docs => {
+      res.json({ success: true, result: docs });
+    })
+    .catch(err => {
+      next(err);
+      return;
+    });
+});
